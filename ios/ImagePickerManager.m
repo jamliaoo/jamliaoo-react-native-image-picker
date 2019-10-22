47554e5b05610f2afbf73734425dce08f8b7bb06
edwardlai3582@gmail.com <edwardlai3582@gmail.com>
2019-10-22 15:16:44 +0800

    fix(ios): add pr#1118 ios13 video issue

--- ios/ImagePickerManager.m
+++ ios/ImagePickerManager.m
@@ -460,7 +460,13 @@ RCT_EXPORT_METHOD(showImagePicker:(NSDictionary *)options callback:(RCTResponseS
 
                 if (videoURL) { // Protect against reported crash
                   NSError *error = nil;
-                  [fileManager moveItemAtURL:videoURL toURL:videoDestinationURL error:&error];
+                  // edl: https://github.com/react-native-community/react-native-image-picker/pull/1118
+                // If we have write access to the source file, move it. Otherwise use copy. 
+                  if ([fileManager isWritableFileAtPath:[videoURL path]]) {
+                    [fileManager moveItemAtURL:videoURL toURL:videoDestinationURL error:&error];
+                  } else {
+                    [fileManager copyItemAtURL:videoURL toURL:videoDestinationURL error:&error];
+                  }
                   if (error) {
                       self.callback(@[@{@"error": error.localizedFailureReason}]);
                       return;
